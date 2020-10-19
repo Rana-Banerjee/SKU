61c3424865eba4c51b577c2de86cb6c01eaca867
Rana-Banerjee <rana1224@gmail.com>
2020-10-19 06:12:33 +0000

    third

--- main.py
+++ main.py
@@ -19,3 +19,4 @@ def predict(url:str):
 #    # This is used when running locally. Gunicorn is used to run the
 #    # application on Google App Engine. See entrypoint in app.yaml.
 #    app.run(host='127.0.0.1', port=8080, debug=True)
+#    uvicorn main:app --reload	
